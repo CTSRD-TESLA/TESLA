2f636a96a02c491b521fd51ff1d33fe14f98df55
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2011-03-13 12:32:50 +0000

    Add sample hand-crafted TESLA assertion "audit", which implements:
      (tesla_assert in VOP_WRITE) -> eventually(invoked(audit_submit))
    
    libtesla now seems to be capable of supporting complex previously
    expressions, and somewhat less complex eventually expressions.
    
    The test program exercises various cases to check that assertions fire
    properly (or properly don't fire).
    
    
    
    svn:r122

---
+++ assertions/audit/audit_assertion.c
@@ -0,0 +1,252 @@
+/*-
+ * Copyright (c) 2011 Robert N. M. Watson
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <sys/types.h>
+
+#ifdef _KERNEL
+#else
+#include <assert.h>
+#include <stdio.h>
+#endif
+
+#include <tesla/tesla_state.h>
+#include <tesla/tesla_util.h>
+
+#include "audit_defs.h"
+
+/*
+ * State associated with this assertion in flight.
+ */
+static struct tesla_state	*audit_state;
+
+/*
+ * This assertion has two automata: an implied system call automata, and the
+ * explicit autaomata described by audit_automata_states and
+ * audit_automata_rules.  For more complex (multi-clause) assertions, there
+ * would be an additional automata for each clause.
+ *
+ * Note: non-zero constants.
+ */
+#define	AUDIT_AUTOMATA_SYSCALL		1	/* In a system call. */
+#define	AUDIT_AUTOMATA_ASSERTION	2	/* Assertion clause. */
+
+/*
+ * Define the maximum number of instances of the assertion to implement
+ * per-thread.  Should be prime, and must be at least 2 so that the system
+ * call automata works.
+ */
+#define	AUDIT_LIMIT	3
+
+/*
+ * Strings used when printing assertion failures.
+ */
+#define	AUDIT_NAME		"audit_submit_check"
+#define	AUDIT_DESCRIPTION	"VOP_WRITE without eventual audit_submit"
+
+/*
+ * When an assertion is initialised, register state management with the TESLA
+ * state framework. This assertion uses per-thread state, since assertions are
+ * relative to specific threads.  Later use of tesla_instance will return
+ * per-thread instances, and synchronisation is avoided.
+ */
+void
+audit_init(void)
+{
+	int error;
+
+	/*
+	 * XXXRW: More realistically, would be TESLA_SCOPE_PERTHREAD, but
+	 * that doesn't work yet.
+	 */
+	error = tesla_state_new(&audit_state, TESLA_SCOPE_GLOBAL, AUDIT_LIMIT,
+	    AUDIT_NAME, AUDIT_DESCRIPTION);
+#ifdef _KERNEL
+	if (error)
+		panic("audit_init: tesla_state_new failed due to %s",
+		    tesla_strerror(error));
+#else
+	assert(error == 0);
+#endif
+#if 0
+	/*
+	 * In the future, this will somehow register instrumentation?  How to
+	 * ensure this is (relatively) atomic?
+	 */
+	TESLA_INSTRUMENTATION(audit_state, tesla_syscall_enter,
+	    audit_event_syscall_enter);
+	TESLA_INSTRUMENTATION(audit_state, tesla_syscall_return,
+	    audit_event_syscall_return);
+	TESLA_INSTRUMENTATION(audit_state, tesla_audit_submit,
+	    audit_event_audit_submit);
+	TESLA_INSTRUMENTATION(audit_state, tesla_214872348923_assertion,
+	    audit_event_assertion);
+#endif
+}
+/* SYSINIT(..., audit_init, ...); */
+
+/*
+ * When the checker is unloaded, GC its state.  Hopefully also un-instruments.
+ */
+void
+audit_destroy(void)
+{
+
+	tesla_state_destroy(audit_state);
+}
+/* SYSUNINIT(..., audit_destroy, ...); */
+
+/*
+* System call enters: prod implicit system call lifespan state machine.
+*/
+void
+audit_event_tesla_syscall_enter(void)
+{
+	struct tesla_instance *tip;
+	int error;
+
+	error = tesla_instance_get1(audit_state, AUDIT_AUTOMATA_SYSCALL,
+	    &tip);
+	if (error)
+		return;
+	tip->ti_state[0] = 1;		/* In syscall. */
+	tesla_instance_put(audit_state, tip);
+}
+
+/*
+ * Used when iterating over eventually expressions in system call return.
+ */
+static void
+audit_iterator_callback(struct tesla_instance *tip, void *arg)
+{
+	u_int event = *(u_int *)arg;
+
+	if (audit_automata_prod(tip, event))
+		tesla_assert_fail(audit_state, tip);
+}
+
+/*
+ * System call returns: prod implicit system call lifespan state machine,
+ * prod eventually clauses, and flush all assertions.
+ */
+void
+audit_event_tesla_syscall_return(void)
+{
+	struct tesla_instance *tip;
+	u_int event;
+	int error;
+
+	error = tesla_instance_get1(audit_state, AUDIT_AUTOMATA_SYSCALL, &tip);
+	if (error)
+		goto out;
+	if (tip->ti_state[0] == 1)
+		tip->ti_state[0] = 0;
+	tesla_instance_put(audit_state, tip);
+
+	/* Prod each eventually automata. */
+	event = AUDIT_EVENT_TESLA_SYSCALL_RETURN;
+	tesla_instance_foreach1(audit_state, AUDIT_AUTOMATA_ASSERTION,
+	    audit_iterator_callback, &event);
+out:
+	tesla_state_flush(audit_state);
+}
+
+/*
+ * Invocation of audit_submit(): prod the state machine.
+ */
+void
+audit_event_audit_submit(void)
+{
+	struct tesla_instance *tip;
+	u_int state;
+	int error;
+
+	error = tesla_instance_get1(audit_state, AUDIT_AUTOMATA_SYSCALL,
+	    &tip);
+	if (error)
+		return;
+	state = tip->ti_state[0];
+	tesla_instance_put(audit_state, tip);
+	if (state != 1)
+		return;
+
+	error = tesla_instance_get1(audit_state, AUDIT_AUTOMATA_ASSERTION,
+	    &tip);
+	if (error)
+		return;
+	if (audit_automata_prod(tip, AUDIT_EVENT_AUDIT_SUBMIT))
+		tesla_assert_fail(audit_state, tip);
+	tesla_instance_put(audit_state, tip);
+}
+
+/*
+* The event implied by the assertion; executes at that point in VOP_WRITE.
+*/
+void
+audit_event_assertion(void)
+{
+	struct tesla_instance *tip;
+	int error, state;
+
+	/*
+	 * Check that we are in a system call; if not, we may have incomplete
+	 * data.
+	 */
+	error = tesla_instance_get1(audit_state, AUDIT_AUTOMATA_SYSCALL, &tip);
+	if (error)
+		return;
+	state = tip->ti_state[0];
+	tesla_instance_put(audit_state, tip);
+	if (state != 1)
+		return;
+
+	error = tesla_instance_get1(audit_state, AUDIT_AUTOMATA_ASSERTION,
+	    &tip);
+	if (error)
+		return;
+	if (audit_automata_prod(tip, AUDIT_EVENT_ASSERTION))
+		tesla_assert_fail(audit_state, tip);
+	tesla_instance_put(audit_state, tip);
+}
+
+static void
+audit_debug_callback(struct tesla_instance *tip)
+{
+
+	printf("%s: assertion %s failed\n", AUDIT_NAME, AUDIT_DESCRIPTION);
+}
+
+void
+audit_setaction_debug(void)
+{
+
+	tesla_state_setaction(audit_state, audit_debug_callback);
+}
---
+++ assertions/audit/audit_automata.c
@@ -0,0 +1,150 @@
+/*-
+ * Copyright (c) 2011 Robert N. M. Watson
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <sys/types.h>
+
+#ifdef _KERNEL
+#else
+#include <assert.h>
+#include <stdlib.h>
+#endif
+
+#include <tesla/tesla_state.h>
+#include <tesla/tesla_util.h>
+
+#include "audit_defs.h"
+
+/*
+ * This is a hand-crafted automata implementing the TESLA assertion:
+ *
+ * (assertion in VOP_WRITE) -> eventually(invoked(audit_submit()))
+ *
+ * Many instances of the automata may be in flight at once during a system
+ * call, instantiated by invocations of tesla_assert().
+ *
+ * The automata recognizes !(assertion), so has a "reject" state rather than
+ * an "accept" state.  The assertion matches as soon as we hit a reject
+ * state.  This is a simple, one-clause previously assertion, so there is
+ * just one automata, no composition required.
+ *
+ * (0) --tesla_assert-> (1) --audit_submit()-> (2) (loop)
+ *  \                    \
+ *   \                    tesla_syscall_return() --> (reject (3)) --> (4) (loop)
+ *     --*-> (0)
+ *
+ * Note that the assertion will fire once in state 3 and then move onto state
+ * 4, where it lives indefinitely.  This ensures that if firing the assertion
+ * is non-fatal, such as when using assertions to trigger DTrace probes, we
+ * fire only once.
+ */
+static struct audit_automata_state {
+	int	aas_reject;
+} audit_automata_states[] = {
+	/* 0 */	{ 0 },	/* Awaiting tesla_assert() */
+	/* 1 */	{ 0 },	/* Awaiting audit_submit() */
+	/* 2 */ { 0 },	/* Termination state */
+	/* 3 */	{ 1 },	/* Reject */
+	/* 4 */ { 0 },	/* Loop after reject */
+};
+static int audit_automata_state_count = sizeof(audit_automata_states) /
+    sizeof(audit_automata_states[0]);
+
+static struct audit_automata_rule {
+	u_int	aar_fromstate;
+	u_int	aar_input;
+	u_int	aar_tostate;
+} audit_automata_rules[] = {
+	{ 0, AUDIT_EVENT_ASSERTION, 1 },		/* Assertion. */
+	{ 0, AUDIT_EVENT_AUDIT_SUBMIT, 0 },		/* Loop in 0. */
+	{ 0, AUDIT_EVENT_TESLA_SYSCALL_RETURN, 0 },	/* Loop in 0. */
+	{ 1, AUDIT_EVENT_AUDIT_SUBMIT, 2 },		/* Satisfied. */
+	{ 1, AUDIT_EVENT_ASSERTION, 1 },		/* Double assertion. */
+	{ 1, AUDIT_EVENT_TESLA_SYSCALL_RETURN, 3 },	/* Submit missed! */
+	{ 2, AUDIT_EVENT_ASSERTION, 1 },		/* Restart. */
+	{ 2, AUDIT_EVENT_AUDIT_SUBMIT, 2 },		/* Double submit. */
+	{ 2, AUDIT_EVENT_TESLA_SYSCALL_RETURN, 2 },	/* Loop in 2. */
+	{ 3, AUDIT_EVENT_ASSERTION, 1 },		/* Fire, Restart. */
+	{ 3, AUDIT_EVENT_AUDIT_SUBMIT, 4 },		/* Loop in 4. */
+	{ 3, AUDIT_EVENT_TESLA_SYSCALL_RETURN, 4 },	/* Loop in 4. */
+	{ 4, AUDIT_EVENT_ASSERTION, 4 },		/* Loop in 4. */
+	{ 4, AUDIT_EVENT_AUDIT_SUBMIT, 4},		/* Loop in 4. */
+	{ 4, AUDIT_EVENT_TESLA_SYSCALL_RETURN, 4},	/* Loop in 4. */
+};
+static int audit_automata_rule_count = sizeof(audit_automata_rules) /
+    sizeof(audit_automata_rules[0]);
+
+/*
+ * This automata uses only ti_state[0].
+ */
+#define	STATE(tip)	((tip)->ti_state[0])
+
+static __inline struct audit_automata_rule *
+audit_automata_lookup_rule(u_int state, u_int event)
+{
+	u_int i;
+
+	for (i = 0; i < audit_automata_rule_count; i++) {
+		if (audit_automata_rules[i].aar_fromstate == state &&
+		    audit_automata_rules[i].aar_input == event)
+			return (&(audit_automata_rules[i]));
+	}
+	return (NULL);
+}
+
+/*
+ * Prod an instance of the AUDIT automata, return (1) if an assertion should
+ * fire, or to let things continue gliding along.
+ */
+int
+audit_automata_prod(struct tesla_instance *tip, u_int event)
+{
+	struct audit_automata_rule *aarp;
+	u_int newstate;
+
+#ifdef _KERNEL
+	KASSERT(STATE(tip) < audit_automata_state_count,
+	    ("audit_automata_prod: invalid state %d", STATE(tip)));
+#else
+	assert(STATE(tip) < audit_automata_state_count);
+#endif
+	aarp = audit_automata_lookup_rule(STATE(tip), event);
+#ifdef _KERNEL
+	KASSERT(aarp != NULL,
+	    ("audit_automata_prod: event %d not accepted", event));
+#else
+	assert(aarp != NULL);
+#endif
+	STATE(tip) = newstate = aarp->aar_tostate;
+	if (audit_automata_states[newstate].aas_reject)
+		return (1);
+	return (0);
+}
