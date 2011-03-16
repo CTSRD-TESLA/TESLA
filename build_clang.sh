9a670c82b83fdcad00782a9076f77d86dc8691e7
Anil Madhavapeddy <anil@recoil.org>
2011-03-16 15:53:37 +0000

    add shell script to build our modified llvm/clang toolchain
    svn:r180

---
+++ build_clang.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Build our patched LLVM/CLANG
+# Run from ctsrd.svn/tesla/trunk
+
+if [ ! -d llvm ]; then
+  git clone http://github.com/trombonehero/llvm
+  cd llvm/tools
+  git clone http://github.com/trombonehero/clang
+  cd ../../
+fi
+
+mkdir -p build
+cd build
+cmake -j5 -DCLANG_BUILD_EXAMPLES=true ../llvm
+make
