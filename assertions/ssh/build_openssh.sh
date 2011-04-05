e21e35226de8caf43a9f2310f86f74270aa0a84e
Steven Murdoch <Steven.Murdoch@cl.cam.ac.uk>
2011-04-05 17:10:35 +0000

    Enhancements and fixes to assertions/ssh build scripts
    - Allow passing of flags to ./configure (needed to skip OpenSSL version
      check with --without-openssl-header-check flag)
    - Fix compilation error due to being in the wrong directory
    - If ./configure or make fails, exit immediately
    - If build is incomplete (indicated by the absence of .build_complete),
      retry it next time the script is run
    - distclean target which removes directories created by build_openssh.sh
    
    svn:r497

--- assertions/ssh/build_openssh.sh
+++ assertions/ssh/build_openssh.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 # Build OpenSSH with clang
+#
+# If any options need to be passed to ./configure, put them in the
+# CONFIGFLAGS environment variable, e.g.
+#  CONFIGFLAGS=--without-openssl-header-check ./build_openssh.sh
+
 set -x
 V="5.8p1"
+
+# Download OpenSSH if needed
 
 if [ ! -e openssh-${V}.tar.gz ]; then
   ftp ftp://www.mirrorservice.org/sites/ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${V}.tar.gz
 fi
 
+# Extract and build with GCC
+
 if [ ! -d openssh-${V}-with-gcc ]; then
   tar -zxf openssh-${V}.tar.gz
   mv openssh-${V} openssh-${V}-with-gcc
-  cd openssh-${V}-with-gcc && ./configure && make
+fi
+
+if [ ! -e openssh-${V}-with-gcc/.build_complete ]; then
+  cd openssh-${V}-with-gcc
+  (./configure $CONFIGFLAGS && make) || exit $?
+  touch .build_complete
   cd ..
 fi
 
+# Extract and build with clang, using TESLA
 
 if [ ! -d openssh-${V}-with-clang-tesla ]; then
   tar -zxf openssh-${V}.tar.gz
@@ -21,11 +36,19 @@ if [ ! -d openssh-${V}-with-clang-tesla ]; then
   cd openssh-${V}-with-clang-tesla
   cp ../instrumentation.spec .
   patch -p0 < ../openssh-teal.patch
-  CC=`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' ../../../kernel/tesla-clang`
-  export CC
-  LIBS=-lteslassh LDFLAGS=-L.. ./configure && make
   cd ..
 fi
+
+if [ ! -e openssh-${V}-with-clang-tesla/.build_complete ]; then
+  cd openssh-${V}-with-clang-tesla
+  CC=`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' ../../../kernel/tesla-clang`
+  export CC
+  (LIBS=-lteslassh LDFLAGS=-L.. ./configure $CONFIGFLAGS && make) || exit $?
+  touch .build_complete
+  cd ..
+fi
+
+# Extract and build with clang, using a fake TESLA
 
 if [ ! -d openssh-${V}-with-clang-faketesla ]; then
   tar -zxf openssh-${V}.tar.gz
@@ -33,7 +56,15 @@ if [ ! -d openssh-${V}-with-clang-faketesla ]; then
   cd openssh-${V}-with-clang-faketesla
   cp ../instrumentation.spec .
   patch -p0 < ../openssh-teal.patch
-  CC=`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' ../.../../kernel/tesla-clang`
-  LIBS=-lfaketeslassh LDFLAGS=-L.. ./configure && make
   cd ..
 fi
+
+if [ ! -e openssh-${V}-with-clang-faketesla/.build_complete ]; then
+  cd openssh-${V}-with-clang-faketesla
+  CC=`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' ../../../kernel/tesla-clang`
+  (LIBS=-lfaketeslassh LDFLAGS=-L.. ./configure $CONFIGFLAGS && make) || exit $?
+  touch .build_complete
+  cd ..
+fi
+
+echo "Build of OpenSSH completed successfully"
