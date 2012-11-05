a5782d197a23df158c0df7571ea51cfb5bd7cf67
Jonathan Anderson <jonathan.anderson@ieee.org>
2012-11-05 09:38:45 +0000

    Split libtesla into src/ and test/ directories.
    I want to start writing libtesla tests to drive its further development; provide a place for these tests to go.
    
    Also, start writing fork.c, which describes how I want automata-forking behaviour to work.

---
+++ libtesla/test/fork.c
@@ -0,0 +1,45 @@
+/**
+ * @file fork.c
+ * Tests automata-forking behaviour.
+ *
+ * When an event is fired, it must be delivered to all automata instances that
+ * care about it, even if they don't exist yet.
+ *
+ * We will support this use case by forking automata: an automaton with a
+ * non-specific name like (x,y,ANY) can be forked off into a more specific
+ * automaton named (x,y,z).
+ *
+ * Forking should happen transparently to the event-translation code: once an
+ * event is translated into foo(x,y,z), the lookup of (x,y,z) automatically
+ * triggers a fork, and the event 'foo' is only dispatched to the automaton
+ * named (x,y,z). After that, (x,y,z) and (x,y,ANY) will be in different
+ * states.
+ *
+ * This test ensures that automata forking works correctly.
+ */
+
+#include <tesla/tesla_state.h>
+#include <tesla/tesla_util.h>
+
+#include <stdio.h>
+
+
+int
+main(int argc, char **argv)
+{
+	printf("Creating new TESLA class... ");
+	int err;
+	struct tesla_state *class;
+	err = tesla_state_new(&class, TESLA_SCOPE_PERTHREAD, 23,
+		"classA", "a class of TESLA automata");
+	if (err) {
+		printf("error: '%s'\n", tesla_strerror(err));
+		return 1;
+	}
+	printf("done.\n");
+
+	/* TODO: write more libtesla code to actually support forking */
+
+	return 0;
+}
+
