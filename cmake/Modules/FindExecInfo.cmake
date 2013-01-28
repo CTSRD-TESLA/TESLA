7356e6e2da08938fd440ef0425f317d2bd9e9aae
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-01-28 17:05:51 +0000

    CMake support for execinfo.
    On Mac OS X, execinfo.h is automatically in /usr/include and libc. On FreeBSD, we need to go looking for it.

---
+++ cmake/Modules/FindExecInfo.cmake
@@ -0,0 +1,14 @@
+if (EXECINFO_INCLUDE_DIRS)
+  set (EXECINFO_FIND_QUIETLY TRUE)
+endif (EXECINFO_INCLUDE_DIRS)
+
+find_path (EXECINFO_INCLUDE_DIRS execinfo.h)
+find_library (EXECINFO_LIBRARY_DIRS NAMES execinfo)
+
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args(EXECINFO DEFAULT_MSG
+  EXECINFO_LIBRARY_DIRS
+  EXECINFO_INCLUDE_DIRS
+)
+
+mark_as_advanced (EXECINFO_LIBRARY_DIRS EXECINFO_INCLUDE_DIRS)
