ba845631c1c8279b25681e4154a89de16ad38239
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-02-12 12:58:58 +0000

    FindExecInfo finds the library, not a directory.

--- cmake/Modules/FindExecInfo.cmake
+++ cmake/Modules/FindExecInfo.cmake
@@ -3,12 +3,12 @@ if (EXECINFO_INCLUDE_DIRS)
 endif (EXECINFO_INCLUDE_DIRS)
 
 find_path (EXECINFO_INCLUDE_DIRS execinfo.h)
-find_library (EXECINFO_LIBRARY_DIRS NAMES execinfo)
+find_library (EXECINFO_LIBRARY NAMES execinfo)
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EXECINFO DEFAULT_MSG
-  EXECINFO_LIBRARY_DIRS
+  EXECINFO_LIBRARY
   EXECINFO_INCLUDE_DIRS
 )
 
-mark_as_advanced (EXECINFO_LIBRARY_DIRS EXECINFO_INCLUDE_DIRS)
+mark_as_advanced (EXECINFO_LIBRARY EXECINFO_INCLUDE_DIRS)
