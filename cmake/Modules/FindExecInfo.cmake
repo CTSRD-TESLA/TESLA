b7e1676005e1089baa7a46e57576eb125f399b1d
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-02-18 15:28:25 +0000

    Fix style of FindExecInfo.cmake.
    Make this file look more like a normal CMake find module.

--- cmake/Modules/FindExecInfo.cmake
+++ cmake/Modules/FindExecInfo.cmake
@@ -1,14 +1,23 @@
-if (EXECINFO_INCLUDE_DIRS)
-  set (EXECINFO_FIND_QUIETLY TRUE)
-endif (EXECINFO_INCLUDE_DIRS)
+# - Find execinfo
+# Find execinfo, which (if not provided with libc/libSystem) provides
+# functions like backtrace().
+#
+# This module defines the following variables:
+#     LIBEXECINFO_FOUND         - whether or not we found execinfo
+#     LIBEXECINFO_INCLUDE_DIR   - include directory containing execinfo.h
+#     LIBEXECINFO_INCLUDE_DIRS  - include directories, including dependencies
+#
 
-find_path (EXECINFO_INCLUDE_DIRS execinfo.h)
+find_path (EXECINFO_INCLUDE_DIR execinfo.h)
 find_library (EXECINFO_LIBRARY NAMES execinfo)
+
+# execinfo doesn't depend on anything
+set(EXECINFO_INCLUDE_DIRS "${EXECINFO_INCLUDE_DIR}")
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EXECINFO DEFAULT_MSG
   EXECINFO_LIBRARY
-  EXECINFO_INCLUDE_DIRS
+  EXECINFO_INCLUDE_DIR
 )
 
-mark_as_advanced (EXECINFO_LIBRARY EXECINFO_INCLUDE_DIRS)
+mark_as_advanced (EXECINFO_LIBRARY EXECINFO_INCLUDE_DIR)
