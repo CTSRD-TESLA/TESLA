4d1b262a4047a968339d4055c6a5c5ecd3bdab7a
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-03-05 20:24:08 +0000

    Added a missing check.
    Now that Argument also has an int_value field, we need to check it when testing equality of Argument objects.

--- tesla/common/Transition.h
+++ tesla/common/Transition.h
@@ -201,6 +201,8 @@ inline bool operator==(const Argument &A1, const Argument &A2) {
     if (A2.has_name() && (A1.name() != A1.name())) return false;
   if (A1.has_value())
     if (A2.has_value() && (A1.value() != A1.value())) return false;
+  if (A1.has_int_value())
+    if (A2.has_int_value() && (A1.int_value() != A1.int_value())) return false;
   return true;
 }
 inline bool operator!=(const Argument &A1, const Argument &A2) {
