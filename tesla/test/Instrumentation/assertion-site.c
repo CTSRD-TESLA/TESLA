2ed613334b0be7d20c8fccd3e21e2bbbb9841ec0
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-03-15 23:27:46 +0000

    Add test for assertion site instrumentation.

---
+++ tesla/test/Instrumentation/assertion-site.c
@@ -0,0 +1,40 @@
+//! @file assertion-site.c  Tests instrumentation of assertion sites.
+/*
+ * Commands for llvm-lit:
+ * RUN: tesla analyse %s -o %t.tesla -- %cflags
+ * RUN: clang -S -emit-llvm %cflags %s -o %t.ll
+ * RUN: tesla instrument -S -tesla-manifest %t.tesla %t.ll -o %t.instr.ll
+ * RUN: FileCheck -input-file %t.instr.ll %s
+ */
+
+#include "tesla-macros.h"
+
+void	foo(void);
+void	bar(void);
+
+int
+main(int argc, char *argv[])
+{
+	// CHECK-NOT: call void {{.*}}@__tesla_inline_assertion
+	// CHECK: call void @__tesla_instrumentation_assertion_reached_0()
+	// CHECK-NOT: call void {{.*}}@__tesla_inline_assertion
+	TESLA_PERTHREAD(since(called(main),
+		TSEQUENCE(
+			callee(called(foo)),
+			callee(returned(bar))
+		)
+	));
+
+	// CHECK-NOT: call void {{.*}}@__tesla_inline_assertion
+	// CHECK: call void @__tesla_instrumentation_assertion_reached_1()
+	// CHECK-NOT: call void {{.*}}@__tesla_inline_assertion
+	TESLA_PERTHREAD(since(called(main),
+		TSEQUENCE(
+			caller(called(foo)),
+			caller(returned(bar))
+		)
+	));
+
+	return 0;
+}
+
