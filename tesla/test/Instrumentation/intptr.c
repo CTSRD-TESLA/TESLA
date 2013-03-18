0cd2de42b919468037da895d2bafc42d85f54770
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-03-18 19:53:19 +0000

    Test int-to-pointer conversion.
    We actually get this wrong: we might expect an 'i32 i' to be defined in a function, but if it's allocated on the stack, %i is actually a pointer to what C calls 'i'!

---
+++ tesla/test/Instrumentation/intptr.c
@@ -0,0 +1,88 @@
+//! @file intptr.c  Tests int-to-pointer conversion for instrumentation.
+/*
+ * Commands for llvm-lit:
+ * RUN: tesla analyse %s -o %t.tesla -- %cflags
+ * RUN: clang -S -emit-llvm %cflags %s -o %t.ll
+ * RUN: tesla instrument -S -tesla-manifest %t.tesla %t.ll -o %t.instr.ll
+ * RUN: clang %ldflags %t.instr.ll -o %t
+ * RUN: %t > %t.out
+ * RUN: FileCheck -input-file %t.out %s
+ *
+ * XFAIL: *
+ */
+
+#include "tesla-macros.h"
+
+#include <assert.h>
+
+struct something
+{
+	int i;
+};
+
+static int
+foo(int x, int *xp, struct something *sp)
+{
+	assert(*xp == x);
+	assert(sp->i == x);
+	return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+	/*
+	 * First we enter main():
+	 *
+	 * CHECK: ====
+	 * CHECK: tesla_update_state()
+	 * CHECK: transitions:  [ (0:0x0 -> 1 <init>) ]
+	 * CHECK: key:          0x0 [ X X X X ]
+	 * CHECK: ====
+	 */
+
+	int i = 4;
+	int *ip = &i;
+
+	struct something s;
+	s.i = i;
+
+	struct something *sp = &s;
+
+
+	/*
+	 * Then we call foo():
+	 *
+	 * CHECK: ====
+	 * CHECK: tesla_update_state()
+	 * CHECK: transitions:  [ (1:0x0 -> 2) ]
+	 * CHECK: key:          0x7 [ 4 [[IP:[0-9a-f]+]] [[SP:[0-9a-f]+]] X ]
+	 * CHECK: ====
+	 */
+
+	assert(foo(i, ip, sp) == 0);
+
+	/*
+	 * Then we hit the NOW event:
+	 *
+	 * CHECK: ====
+	 * CHECK: tesla_update_state()
+	 * CHECK: transitions:  [ (2:0x7 -> 3) ]
+	 * CHECK: key:          0x7 [ 4 [[IP]] [[SP]] X ]
+	 * CHECK: ====
+	 */
+	TESLA_WITHIN(main, previously(foo(i, ip, sp) == 0));
+
+	/*
+	 * And finally we leave main():
+	 *
+	 * CHECK: ====
+	 * CHECK: tesla_update_state()
+	 * CHECK: transitions:  [ (3:0x7 -> 4 <clean>) ]
+	 * CHECK: key:          0x0 [ X X X X ]
+	 * CHECK: ====
+	 */
+
+	return 0;
+}
+
