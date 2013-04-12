bebc52cc2dca0b312248e2ef23989a321d03c1bb
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-04-12 12:35:51 +0100

    Create a tesla-cat tool.
    This tool merges several .tesla files into a single ManifestFile object within an output .tesla file.

---
+++ tesla/test/Automata/cat.c
@@ -0,0 +1,78 @@
+/*
+ * Test that tesla-cat can put multiple .tesla files together.
+ *
+ * Commands for llvm-lit (abusing the C preprocessor a bit):
+ * RUN: clang -E -D FILE_A %s -o %t.a.tesla
+ * RUN: clang -E -D FILE_B %s -o %t.b.tesla
+ * RUN: tesla cat %t.a.tesla %t.b.tesla -o %t.tesla
+ * RUN: FileCheck %s -input-file %t.tesla
+ */
+
+
+#ifdef FILE_A
+identifier {
+// CHECK: name: "assertion_a"
+  name: "assertion_a"
+}
+context: Global
+expression {
+  type: FIELD_ASSIGN
+  fieldAssign {
+    type: "tcpcb"
+    index: 5
+    base {
+      type: Variable
+      index: 0
+      name: "tp"
+    }
+    operation: SimpleAssign
+    value {
+      type: Constant
+      name: "TCPS_CLOSED"
+      value: 0
+    }
+    fieldName: "t_state"
+  }
+}
+argument {
+  type: Variable
+  index: 0
+  name: "tp"
+}
+===
+#endif
+
+
+#ifdef FILE_B
+identifier {
+// CHECK: name: "assertion_b"
+  name: "assertion_b"
+}
+context: Global
+expression {
+  type: FIELD_ASSIGN
+  fieldAssign {
+    type: "tcpcb"
+    index: 5
+    base {
+      type: Variable
+      index: 0
+      name: "tp"
+    }
+    operation: SimpleAssign
+    value {
+      type: Constant
+      name: "TCPS_ESTABLISHED"
+      value: 4
+    }
+    fieldName: "t_state"
+  }
+}
+argument {
+  type: Variable
+  index: 0
+  name: "tp"
+}
+===
+#endif
+
