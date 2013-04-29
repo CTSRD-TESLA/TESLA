b8f9177f90aed9ad8043af22756626bc8bcce88d
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-04-29 15:25:57 +0100

    Clarify console output of transitions.
    A little bit of parenthesis can go a long way towards making these transitions more readable at the console.

--- tesla/common/Transition.cpp
+++ tesla/common/Transition.cpp
@@ -212,9 +212,9 @@ string Transition::String() const {
     ;
 
   return (Twine()
-    + "--"
+    + "--("
     + ShortLabel()
-    + "-->("
+    + ")-->("
     + Twine(To.ID())
     + (Special.empty() ? "" : " " + Special)
     + ")"
