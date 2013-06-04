d99dd20ecd779a6322bc6f0812975ab84443b396
Brooks Davis <brooks@one-eyed-alien.net>
2013-06-04 20:43:33 +0000

    In the kernel ignore HAVE_STRNLEN and just include sys/libkern.h which defines strnlen.

--- libtesla/src/tesla_strnlen.h
+++ libtesla/src/tesla_strnlen.h
@@ -2,13 +2,14 @@
 #include "config.h"
 #endif
 
+#ifdef _KERNEL
+#include <sys/libkern.h>
+#else
 #ifdef HAVE_STRNLEN
-#ifndef _KERNEL
 #include <string.h>
-#endif
-
 #else
 /* If we don't have strnlen(), fake it. */
 #warning Platform does not supply strnlen(); faking it with strlen().
 #define strnlen(s, len) strlen(s)
+#endif
 #endif
