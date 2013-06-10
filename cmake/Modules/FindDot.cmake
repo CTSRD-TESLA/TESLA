b42efaad0706fef7a0b4cc3dbe47afdb151302f9
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-06-10 16:58:13 +0100

    Flesh out 'dot' checks.

--- cmake/Modules/FindDot.cmake
+++ cmake/Modules/FindDot.cmake
@@ -1,4 +1,5 @@
 ## Copyright (c) 2011, David Pineau
+## Copyright (c) 2013 Jonathan Anderson
 ## All rights reserved.
 
 ## Redistribution and use in source and binary forms, with or without
@@ -24,7 +25,7 @@
 ## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ## POSSIBILITY OF SUCH DAMAGE.
 
-IF (NOT DEFINED DOT_EXECUTABLE)
+if (NOT DOT_EXECUTABLE_FOUND)
   FIND_PROGRAM(DOT_EXECUTABLE
     NAMES dot
     PATHS
@@ -47,9 +48,58 @@ IF (NOT DEFINED DOT_EXECUTABLE)
     DOC "Graphviz Dot tool for generating image graph from dot file"
   )
 
-  IF (DOT_EXECUTABLE)
-    SET (DOT_EXECUTABLE_FOUND TRUE)
-    SET (DOT_EXECUTABLE ${DOT_EXECUTABLE} CACHE FILEPATH "'dot' location")
-    MESSAGE(STATUS "Dot executable found : ${DOT_EXECUTABLE}")
-  ENDIF (DOT_EXECUTABLE)
-ENDIF (NOT DEFINED DOT_EXECUTABLE)
+  if (DOT_EXECUTABLE)
+    message(STATUS "Dot executable found : ${DOT_EXECUTABLE}")
+    set(ERR 0)
+
+    #
+    # Test that the 'dot' we found actually does the right thing.
+    #
+    set(F "${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/test.dot")
+    file(WRITE ${F} "digraph foo { a -> b; } digraph bar { c -> d; }")
+
+    #
+    # First, make sure it doesn't blat its output to stdout.
+    #
+    execute_process(
+      COMMAND ${DOT_EXECUTABLE} -Tpdf ${F} -o ${F}.pdf
+      OUTPUT_VARIABLE STDOUT)
+
+    file(READ ${F}.pdf PDF HEX)
+
+    if (NOT "${STDOUT}" STREQUAL "")
+      set(ERR 1)
+      message(WARNING "dot ignored -o, emitted PDF output to stdout")
+    endif ()
+
+    if ("${PDF}" STREQUAL "")
+      set(ERR 1)
+      message(WARNING "dot -Tpdf produced an empty file")
+    endif ()
+
+    #
+    # Next, make sure we can produce PNG output.
+    #
+    execute_process(
+      COMMAND ${DOT_EXECUTABLE} -Tpng ${F} -o ${F}.png
+      OUTPUT_VARIABLE STDOUT)
+
+    file(READ ${F}.png PNG HEX)
+
+    if ("${PNG}" STREQUAL "")
+      set(ERR 1)
+      message(WARNING "dot -Tpng produced an empty file")
+    endif ()
+
+    #
+    # All checks complete.
+    #
+    if (NOT ERR)
+      message(STATUS "'dot' executable works as expected")
+
+      set (DOT_EXECUTABLE_FOUND TRUE CACHE BOOL "'dot' command found, works")
+      set (DOT_EXECUTABLE ${DOT_EXECUTABLE} CACHE FILEPATH "'dot' location")
+    endif ()
+  endif (DOT_EXECUTABLE)
+
+endif (NOT DOT_EXECUTABLE_FOUND)
