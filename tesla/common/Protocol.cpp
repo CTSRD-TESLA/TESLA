284cfa5844521eeef67ac2d0b7530ac1b23ccaaf
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-07-25 15:52:56 +0100

    Generic protobuf equality checking.
    This will prevent us from making silly errors like forgetting to update an operator==() when we change the protobuf. It will also save work if we add new message types in the future.
    
    This comparison uses reflection, so it requires the full protobuf library (rather than protobuf-lite). That's what we've been using anyway by default, but now it's a hard requirement.

--- tesla/common/Protocol.cpp
+++ tesla/common/Protocol.cpp
@@ -31,7 +31,128 @@
  * SUCH DAMAGE.
  */
 
+#include "Debug.h"
 #include "Protocol.h"
+
+#include <llvm/Support/ErrorHandling.h>
+
+using google::protobuf::FieldDescriptor;
+using google::protobuf::Message;
+using google::protobuf::Reflection;
+
+namespace tesla {
+static bool FieldEq(const Message&, const Message&, const Reflection&,
+                    const FieldDescriptor*, int index = -1);
+}
+
+bool tesla::operator == (const Message& x, const Message& y) {
+  if (x.GetTypeName() != y.GetTypeName())
+    return false;
+
+  auto* r = x.GetReflection();
+
+  std::vector<const FieldDescriptor*> xFields, yFields;
+  r->ListFields(x, &xFields);
+  r->ListFields(y, &yFields);
+
+  if (xFields.size() != yFields.size())
+    return false;
+
+  for (auto f : xFields) {
+    assert(f->type() <= FieldDescriptor::MAX_TYPE);
+
+    if (f->is_repeated()) {
+      const size_t Size = r->FieldSize(x, f);
+
+      if (r->FieldSize(y, f) != Size)
+        return false;
+
+      for (size_t i = 0; i < Size; i++)
+        if (!FieldEq(x, y, *r, f, i))
+          return false;
+
+    } else {
+      if (!r->HasField(y, f))
+        return false;
+
+      if (!FieldEq(x, y, *r, f))
+        return false;
+    }
+  }
+
+  return true;
+}
+
+bool tesla::FieldEq(const Message& x, const Message& y, const Reflection& r,
+                    const FieldDescriptor *field, int i) {
+
+  const bool Repeated = (i >= 0);
+
+  if (Repeated)
+    assert((r.FieldSize(x, field) > i) && (r.FieldSize(y, field) > i));
+  else
+    assert(r.HasField(x, field) && r.HasField(y, field));
+
+  #define GET(message, type, i) \
+    (Repeated \
+      ? r.GetRepeated##type(message, field, i) \
+      : r.Get##type(message, field))
+
+  switch (field->type()) {
+  case FieldDescriptor::TYPE_BOOL:
+    return (GET(x, Bool, i) == GET(y, Bool, i));
+
+  case FieldDescriptor::TYPE_INT32:
+    return (GET(x, Int32, i) == GET(y, Int32, i));
+
+  case FieldDescriptor::TYPE_UINT32:
+    return (GET(x, UInt32, i) == GET(y, UInt32, i));
+
+  case FieldDescriptor::TYPE_INT64:
+    return (GET(x, Int64, i) == GET(y, Int64, i));
+
+  case FieldDescriptor::TYPE_UINT64:
+    return (GET(x, UInt64, i) == GET(y, UInt64, i));
+
+  case FieldDescriptor::TYPE_FLOAT:
+    return (GET(x, Float, i) == GET(y, Float, i));
+
+  case FieldDescriptor::TYPE_DOUBLE:
+    return (GET(x, Double, i) == GET(y, Double, i));
+
+  case FieldDescriptor::TYPE_STRING:
+    return (GET(x, String, i) == GET(y, String, i));
+
+  case FieldDescriptor::TYPE_ENUM: {
+    auto *xe = GET(x, Enum, i), *ye = GET(y, Enum, i);
+    return ((xe->full_name() == ye->full_name())
+            && (xe->number() == ye->number()));
+  }
+
+  case FieldDescriptor::TYPE_MESSAGE: {
+    const Message &xm = GET(x, Message, i), &ym = GET(y, Message, i);
+    return (xm == ym);
+  }
+
+  case FieldDescriptor::TYPE_FIXED32:
+  case FieldDescriptor::TYPE_FIXED64:
+  case FieldDescriptor::TYPE_SFIXED32:
+  case FieldDescriptor::TYPE_SFIXED64:
+    panic("unhandled fixed-point protobuf field");
+
+  case FieldDescriptor::TYPE_GROUP:
+    panic("unhandled (deprecated) 'group' protobuf field");
+
+  case FieldDescriptor::TYPE_BYTES:
+    panic("unhandled 'bytes' protobuf field");
+
+  case FieldDescriptor::TYPE_SINT32:
+  case FieldDescriptor::TYPE_SINT64:
+    panic("unhandled SInt(32)|(64) protobuf field");
+   }
+
+  llvm_unreachable("unhandled protobuf::Message f type");
+}
 
 int tesla::ArgIndex(const Argument& A) {
   switch (A.type()) {
