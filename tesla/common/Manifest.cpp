45c1b7b3e53a0d643b486924ad5191a61833bbad
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-10-09 11:45:30 +0100

    Add some lifetime debug output.

--- tesla/common/Manifest.cpp
+++ tesla/common/Manifest.cpp
@@ -154,6 +154,21 @@ Manifest::load(raw_ostream& ErrorStream, Automaton::Type T, StringRef Path) {
     Automata[ID] = Result.take();
   }
 
+  raw_ostream& debug = debugs("tesla.manifest.lifetimes");
+  debug << "--------\nUnique automata lifetimes:\n";
+  for (auto& Lifetime : Lifetimes) {
+    assert(Lifetime.Init != NULL);
+    assert(Lifetime.Cleanup != NULL);
+
+    debug
+      << " * " << AutomatonDescription::Context_Name(Lifetime.Context)
+      << ": " << Lifetime.Init->ShortLabel()
+      << " -> " << Lifetime.Cleanup->ShortLabel()
+      << "\n"
+      ;
+  }
+  debug << "--------\n";
+
   return new Manifest(Protobuf, Descriptions, Automata, Roots, Lifetimes);
 }
 
