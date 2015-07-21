4e4da020eca9ebbba50ae4d3b99da16bc90478c9
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-07-21 15:50:12 -0230

    Update static function instrumentation test.

--- test/static.c
+++ test/static.c
@@ -1,33 +1,48 @@
-//! @file Instrumentation/static.c   Tests instrumention of static functions.
-/*
- * Commands for llvm-lit:
- * RUN: mkdir -p %t
- * RUN: clang -S -emit-llvm %cflags %s -o %t/main.ll
- * RUN: tesla analyse %s -o %t/tesla -- %cflags
- * RUN: tesla instrument -S -tesla-manifest %t/tesla %t/main.ll -o %t/instr.ll
- * RUN: %filecheck -input-file %t/instr.ll %s
+/*!
+ * \file  static-functions.c
+ * \brief Tests instrumentation of static functions.
  *
- * XFAIL: *
+ * Commands for llvm-lit:
+ * RUN: %cpp -DINSTRUMENTATION_FILE %s > %t.yaml
+ * RUN: %cpp %s > %t.c
+ * RUN: %clang -S -emit-llvm %cflags %t.c -o %t.ll
+ * RUN: %opt -S %t.ll -tesla-file %t.yaml -o %t.instr.ll
+ * RUN: %filecheck -input-file %t.instr.ll %s
  */
 
-#include <tesla-macros.h>
+#if defined (INSTRUMENTATION_FILE)
+
+hook_prefix: __test_hook
+
+functions:
+    - name: foo
+      callee: [ entry, exit ]
+      caller: [ entry, exit ]
+
+#else
+
+static void foo();
+
+
+int
+main(int argc, char *argv[])
+{
+	// CHECK: define i{{[0-9]+}} @main
+
+	// CHECK: call void @[[PREFIX:__test_hook]]_call_foo
+	foo();
+	// CHECK: call void @[[PREFIX]]_return_foo
+
+	return 0;
+}
 
 static void
 foo()
 {
 	/*
 	 * CHECK: define internal void @foo()
-	 * CHECK: call void @__tesla_instrumentation_callee_enter_foo
+	 * CHECK: call void @[[PREFIX]]_enter_foo
+	 * CHECK: call void @[[PREFIX]]_leave_foo
 	 */
 }
-
-
-int
-main(int argc, char *argv[])
-{
-	TESLA_WITHIN(main, eventually(call(foo)));
-
-	foo();
-
-	return 0;
-}
+#endif /* !INSTRUMENTATION_FILE */
